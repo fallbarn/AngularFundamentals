ad80115d609c970ad3d08da23c67dac14b9657a8
PeterDuck <fallbarn@hotmail.com>
2020-06-12 05:08:00 +0100

    Yet more comment

--- src/app/Events-app.component.ts
+++ src/app/Events-app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // sle note: points to the tag index.html (master page) where the first html will be inserted.
+  // sle note: points to the 'Events-app' tag in index.html (master page?) where the first html will be inserted.
   selector: 'Events-app',
-  template: '<h2>Hello World</h2><img src="assets/images/basic-shield.png"/>'
+  template: "<Events-list></Events-list>"
 })
 export class EventsAppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './Events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [EventsAppComponent]
+  bootstrap: [EventsAppComponent, EventsListComponent]
 })
 // sle note: AppModule can't be renamed.
 export class AppModule { }
